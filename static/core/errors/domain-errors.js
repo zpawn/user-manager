96e6b852d02ea1bb47b1794f4d344cdb15f98cfd
ilia makarov <ilia.makarov@me.com>
2025-09-14 16:34:58 +0300

    Revert "Implement DSL for IndexedDBRepository with Fluent API"
    This reverts commit c5ce927c62abab85bd550483428e2741441da212.

--- static/core/errors/domain-errors.js
+++
@@ -1,6 +0,0 @@
-@@ .. @@
- export class DeleteError extends RepositoryError {}
- export class QueryError extends RepositoryError {}
-+export class DSLError extends DomainError {}
-
- // File System specific errors
--- static/core/indexed-db/index.js
+++ static/core/indexed-db/index.js
@@ -1,3 +1,2 @@
 export { IndexedDBRepository } from './indexed-db-repository.js';
 export { IndexedDBStorage } from './indexed-db-storage.js';
-export { IndexedDBQueryBuilder } from './query-builder.js';
